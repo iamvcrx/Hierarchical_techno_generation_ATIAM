fb58ee69f709c8bec597da253bdec6ec2bb7849b
quentin-le-gall <quentin.legall@atiam.fr>
2022-12-01 23:59:04 +0100

    [QLG] Cleanup de mon dossier

--- Quentin_Le_Gall/TECHNO/Utilities.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-class Reshape(torch.nn.Module):
-    def __init__(self, outer_shape):
-        super(Reshape, self).__init__()
-        self.outer_shape = outer_shape
-    def forward(self, x):
-        return x.view(x.size(0), *self.outer_shape)
